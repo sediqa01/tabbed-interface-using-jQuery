f852ba227ec8c679425d299e7ace0a2b3235cc90
Sediqa <sadat.hadid2000@gmail.com>
2024-02-13 12:41:54 +0000

    Fade out the visible tab

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $("#tabs > ul > li > a").click( function(){
     $("#tabs > ul > li > a").css( { "background" : "#A2A2A2", "color" : "#CECECE" } );
     $(this).css( { "background" : "#EAEAEA", "color" : "#333" } );
+
+        const thisTab = $(this).attr("href");
+        $("#tabs > div:visible").fadeOut(200);
  } )
