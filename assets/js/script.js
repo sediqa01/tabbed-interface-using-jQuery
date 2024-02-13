f56c99a0accedf9b592a235147202df482ec5360
Sediqa <sadat.hadid2000@gmail.com>
2024-02-13 13:45:07 +0000

    Add for loop to change the tab class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-var tabs = document.querySelectorAll();
+var tabs = document.querySelectorAll('#tabs > ul > li > a');
 
 for (var i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener('click', selectTab)
 }
 
-function selectTab() {
-    
+function selectTab(event) {
+    event.preventDefault();
+
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].removeAttribute('class');
+    }
+    event.target.className = 'active';
 }
