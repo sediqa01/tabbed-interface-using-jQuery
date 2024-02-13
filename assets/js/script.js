df7c92ab5ea80c34db8d8d148077708af694a7c1
Sediqa <sadat.hadid2000@gmail.com>
2024-02-13 12:54:20 +0000

    use the callback function to fadeIn the div that matches the href for the link was clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@ $("#tabs > ul > li > a").click( function(){
     $("#tabs > ul > li > a").css( { "background" : "#A2A2A2", "color" : "#CECECE" } );
     $(this).css( { "background" : "#EAEAEA", "color" : "#333" } );
 
-        const thisTab = $(this).attr("href");
-        $("#tabs > div:visible").fadeOut(200);
+    const thisTab = $(this).attr("href");git
+    $("#tabs > div:visible").fadeOut(200, function(){
+        $(thisTab).fadeIn(200);
+        } );
+        
  } )
