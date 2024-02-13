7a18fa50fef9961369764ec96d4396a5800b65e0
Sediqa <sadat.hadid2000@gmail.com>
2024-02-13 14:04:15 +0000

    Clean up the code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
-var tabs = document.querySelectorAll('#tabs > ul > li > a');
+const tabs = document.querySelectorAll('#tabs > ul > li > a');
 
-for (var i = 0; i < tabs.length; i++) {
+for (let i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener('click', selectTab)
 }
 
 function selectTab(event) {
     event.preventDefault();
-
-    for (var i = 0; i < tabs.length; i++) {
-        tabs[i].removeAttribute('class');
-    }
+    tabs.forEach(
+        tab => { tab.removeAttribute('class'); 
+    });
     event.target.className = 'active';
+    const thisTab = event.target.getAttribute('href');
+    const thisContent = document.querySelector(thisTab);
+    const oldTabContent = document.querySelector('.visible');
+    oldTabContent.className = 'visuallyhidden';
+
+    oldTabContent.addEventListener('transitionend', () => {
+        oldTabContent.className = 'hidden';
+        thisContent.className = 'visible visuallyhidden';
+        setTimeout( () => {
+        thisContent.classList.remove('visuallyhidden');
+        }, 20);
+
+    }, { capture: false, once: true, passive: false });
 }
